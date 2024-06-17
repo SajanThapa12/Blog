3e514ae0d14e3a47fe32ad572dc5222e3ee27d93
Sajan Thapa <madristasajan@gmail.com>
2024-06-17 20:57:35 +0545

    7th commit

--- app.js
+++ app.js
@@ -23,15 +23,15 @@ const corsOptions = {
   preflightcontinue: true,
 };
 
-app.use(cors(corsOptions));
-app.all("*", function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
-  );
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-});
+// app.use(cors(corsOptions));
+// app.all("*", function (req, res) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+//   );
+//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+// });
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 7000;
